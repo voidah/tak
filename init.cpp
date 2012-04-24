34bb007fe60e0a9eb7bcf1bd9a97639d2e0a3588
Arthur Ouellet <arthur@pwel.org>
2012-04-23 23:45:26 -0400

    Changed default FPS limit to 30

--- init.cpp
+++ init.cpp
@@ -8,8 +8,8 @@ bool RunGame(Game* game)
     Tool::CheckTypes();
 
     Engine engine(game);
-    engine.SetMaxFps(99999);
-    engine.Start(game->GetName(), game->Config.InitialWidth, game->Config.InitialHeight, false, 0);
+    engine.SetMaxFps(60);
+    engine.Start(game->GetName(), game->Config.InitialWidth, game->Config.InitialHeight, false, game->Config.AntialiasingFactor);
 
     return true;
 }
