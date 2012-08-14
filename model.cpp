5c22336da060f22a944660c7b2bf9c7cacdcaeff
Arthur Ouellet <arthur@pwel.org>
2012-08-14 07:28:03 -0400

    Initial model support

---
+++ model.cpp
@@ -0,0 +1,80 @@
+#include "model.h"
+#include <fstream>
+#include <cassert>
+
+Model::Model() : m_vertices(0), m_indices(0), m_vertexCount(0), m_indexCount(0)
+{
+}
+
+Model::~Model()
+{
+}
+
+bool Model::Load(const std::string& key, const OptionList& options)
+{
+    std::ifstream tmd(key.c_str());
+    if(!tmd.is_open())
+        return false;
+
+    tmd >> m_vertexCount >> m_indexCount;
+
+    assert(m_vertexCount < 65535); // Mesh currently uses 16 bit for indexes
+
+    std::cout << "vcount: " << m_vertexCount << ", icount: " << m_indexCount << std::endl;
+
+    m_vertices = new Vertex[m_vertexCount];
+    m_indices = new Index[m_indexCount];
+
+
+    for(int i = 0; i < m_vertexCount; ++i)
+    {
+        Vertex& v = m_vertices[i];
+        tmd >> v.coord.x;
+        tmd >> v.coord.y;
+        tmd >> v.coord.z;
+        tmd >> v.normal.x;
+        tmd >> v.normal.y;
+        tmd >> v.normal.z;
+    }
+
+    for(int i = 0; i < m_indexCount; ++i)
+    {
+        tmd >> m_indices[i];
+    }
+
+    tmd.close();
+    return true;
+}
+
+bool Model::Release()
+{
+    if(m_vertices)
+        delete [] m_vertices;
+
+    if(m_indices)
+        delete [] m_indices;
+    return true;
+}
+
+Mesh<VertexData3d>* Model::GetMesh()
+{
+    assert(m_vertexCount > 0 && m_indexCount > 0);
+
+    VertexData3d* vd = new VertexData3d[m_vertexCount];
+
+    for(int i = 0; i < m_vertexCount; ++i)
+    {
+        Vertex& v = m_vertices[i];
+
+        vd[i] = Mesh<VertexData3d>::VertexData(v.coord.x, v.coord.y, v.coord.z, 1.f, 1.f, 1.f, 0, 0);
+        // TODO add normals..
+    }
+
+    Mesh<VertexData3d>* mesh = new Mesh<VertexData3d>(Mesh<VertexData3d>::MT_TRIANGLE);
+    mesh->SetMeshData(vd, m_vertexCount, m_indices, m_indexCount);
+    //mesh->SetMeshData(vd, m_vertexCount);
+
+    delete [] vd;
+    return mesh;
+}
+
---
+++ model.h
@@ -0,0 +1,37 @@
+#ifndef TAK_MODEL_H__
+#define TAK_MODEL_H__
+
+#include "define.h"
+#include "resource.h"
+#include "vector3.h"
+#include "mesh.h"
+
+class Model : public Resource<Model>
+{
+    public:
+        Model();
+        virtual ~Model();
+
+        virtual bool Load(const std::string& key, const OptionList& options);
+        virtual bool Release();
+
+        Mesh<VertexData3d>* GetMesh();
+
+    private:
+        struct Vertex
+        {
+            Vector3f coord;
+            Vector3f normal;
+        };
+
+        typedef uint16 Index;
+
+    private:
+        Vertex* m_vertices;
+        Index* m_indices;
+
+        int m_vertexCount;
+        int m_indexCount;
+};
+
+#endif // TAK_MODEL_H__
